0d080a0c7708c55f4f550b392fdd2d3cd9afe517
Helton-Carlos <heltoncarlospa>
2026-04-09 13:03:15 -0300

    feat: add dayse UI

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <button
-    class="bg-green-700 text-white px-2 py-2 my-2 rounded hover:bg-green-300 hover:cursor-pointer"
-    @click="$emit('onClick')"
-  >
+  <button class="btn btn-success" @click="$emit('onClick')">
     <slot />
   </button>
 </template>
