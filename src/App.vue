5a45e492a36bae82010d471100915ed56f66dece
Helton-Carlos <heltoncarlospa>
2026-04-09 12:58:29 -0300

    feat: add tailwind 4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button.vue';
+function click() {
+  alert('hey');
+}
 </script>
 
 <template>
-  <HelloWorld />
+  <h1 class="text-5xl font-bold bg-green-700 p-4">vue 3</h1>
+  <Button @onClick="click"> Enviar alerta </Button>
 </template>
---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    class="bg-green-700 text-white px-2 py-2 my-2 rounded hover:bg-green-300 hover:cursor-pointer"
+    @click="$emit('onClick')"
+  >
+    <slot />
+  </button>
+</template>
