5732b1cdfa1397b960c20a5c549c7e66acd5b495
Helton-Carlos <heltoncarlospa>
2026-04-09 17:31:59 -0300

    feat: add dropdown

---
+++ src/components/BaseDropdown.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  itens: { path: string; name: string }[];
+}>();
+</script>
+
+<template>
+  <details class="dropdown">
+    <summary class="btn m-1">
+      {{ title }}
+    </summary>
+    <ul
+      v-for="(item, index) in itens"
+      :key="index"
+      class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+    >
+      <li>
+        <a :href="item.path">
+          {{ item.name }}
+        </a>
+      </li>
+    </ul>
+  </details>
+</template>
