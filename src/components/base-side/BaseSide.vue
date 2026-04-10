4d7fc3f7adf35b70fccb8bf5de441832d919b614
Helton-Carlos <heltoncarlospa>
2026-04-10 16:21:05 -0300

    feat: add iconify

--- src/components/base-side/BaseSide.vue
+++ src/components/base-side/BaseSide.vue
@@ -1,61 +1,50 @@
+<script setup lang="ts">
+import BaseButton from '../base-button/BaseButton.vue';
+import { Icon } from '@iconify/vue';
+</script>
+
 <template>
-  <ul class="menu bg-base-200 rounded-box w-[285px] h-dvh">
-    <li>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+  <div class="menu bg-base-200 rounded-box w-[285px] h-dvh">
+    <h1 class="text-lg font-bold text-center my-8">
+      Testing
+    </h1>
+
+    <ul class="flex-1 mt-8">
+      <li>
+        <a>
+          <Icon
+            class="icon"
+            icon="cil:file"
           />
-        </svg>
-        Item 2
-      </a>
-    </li>
-    <li>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          Item 2
+        </a>
+      </li>
+      <li>
+        <a>
+          <Icon
+            class="icon"
+            icon="cil:locomotive"
           />
-        </svg>
-        Item 1
-      </a>
-    </li>
-    <li>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+          Item 1
+        </a>
+      </li>
+      <li>
+        <a>
+          <Icon
+            class="icon"
+            icon="cil:locomotive"
           />
-        </svg>
-        Item 3
-      </a>
-    </li>
-  </ul>
+          Item 3
+        </a>
+      </li>
+    </ul>
+
+    <BaseButton class="flex items-center gap-4 m-10">
+      <Icon
+        class="icon"
+        icon="cil:exit-to-app"
+      />
+      Sair
+    </BaseButton>
+  </div>
 </template>
