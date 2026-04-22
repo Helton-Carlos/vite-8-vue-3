8394f0f87a2231db0350f3d4f9badb10cab0fbf5
Helton Silva <helton.silva@melhorenvio.com>
2026-04-22 16:52:02 -0300

    feat: add toast

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import { router } from './router';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { router } from "./router";
+import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 
 const app = createApp(App);
-app.use(router).mount('#app');
+app
+  .use(router)
+  .use(Vue3Toastify, {
+    autoClose: 3000,
+  } as ToastContainerOptions)
+  .mount("#app");
--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import BaseButton from "../components/base-button/BaseButton.vue";
 import { useRouter } from "vue-router";
+import { toast } from "vue3-toastify";
 
 const router = useRouter();
 
 function submit() {
+  toast.success("Seja-bem vindo(a)", {
+    progressClassName: "bg-green-500",
+    position: toast.POSITION.BOTTOM_RIGHT,
+  });
+
   router.push({ name: "dashboard" });
 }
 </script>
@@ -21,17 +27,15 @@ function submit() {
         type="email"
         placeholder="Digite seu e-mail"
         class="w-full input focus:input-success"
-      >
+      />
 
       <input
         type="password"
         placeholder="Digite sua senha"
         class="w-full mb-4 input focus:input-success"
-      >
+      />
 
-      <BaseButton type="submit">
-        Sing-in
-      </BaseButton>
+      <BaseButton type="submit"> Sing-in </BaseButton>
     </form>
   </div>
 </template>
