ac3a945ad20b8aea45839446b071fc7976b24f38
Helton-Carlos <heltoncarlospa>
2026-05-01 10:26:21 -0300

    feat: component base badge

---
+++ src/components/base-badge/BaseBadge.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  variant?:
+    | 'primary'
+    | 'secondary'
+    | 'accent'
+    | 'info'
+    | 'success'
+    | 'warning'
+    | 'error'
+    | 'ghost';
+}>();
+
+const variantMap: Record<string, string> = {
+  primary: 'badge-primary',
+  secondary: 'badge-secondary',
+  accent: 'badge-accent',
+  info: 'badge-info',
+  success: 'badge-success',
+  warning: 'badge-warning',
+  error: 'badge-error',
+  ghost: 'badge-ghost',
+};
+</script>
+
+<template>
+  <span class="badge" :class="variantMap[variant || 'ghost']">
+    <slot />
+  </span>
+</template>
