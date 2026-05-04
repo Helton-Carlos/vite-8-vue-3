b8bdf2f8ac649de66aec37db419d423a51a03ebf
Helton Silva <helton.silva@melhorenvio.com>
2026-05-04 16:37:25 -0300

    feat: add test base badge

---
+++ src/__tests__/components/base-badge.spec.ts
@@ -0,0 +1,33 @@
+import { render } from "@testing-library/vue";
+import { describe, expect, test } from "vitest";
+import BaseBadge from "../../components/base-badge/BaseBadge.vue";
+
+describe("BaseBadge.vue", () => {
+  test("renders the badge with default props", async () => {
+    const badge = await render(BaseBadge, {
+      props: {
+        variant: "primary",
+        size: "md",
+        outline: true,
+      },
+    });
+
+    expect(badge).toBeTruthy();
+  });
+
+  test("renders the badge with slots", async () => {
+    const badge = render(BaseBadge, {
+      slots: { default: "My Badge" },
+      props: {
+        variant: "primary",
+        size: "md",
+        outline: true,
+      },
+    });
+
+    const element = badge.getByText("My Badge");
+
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass("badge-outline");
+  });
+});
